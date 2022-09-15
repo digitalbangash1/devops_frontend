5964628159224d51ac9d8c32473a40373f63dca5
digitalbangash1 <s092512@student.dtu.dk>
2022-09-15 08:06:08 +0200

    Navbar updated

--- src/components/shared_view/Navbar.js
+++ src/components/shared_view/Navbar.js
@@ -4,16 +4,17 @@ import React from "react";
 function Navbar(){
     return (
 
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Navbar</a>
+        <nav className="navbar navbar-expand-lg py-4 navbar-light bg-light">
+
+            <a className="navbar-brand px-3" href="#">Navbar</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
 
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
+            <div className="collapse navbar-collapse justify-content-end"  id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto px-2">
                     <li className="nav-item active">
                         <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                     </li>
