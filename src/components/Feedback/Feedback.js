84f3c6429ad23c7449c148c14d11d6e3dd6ea80b
alidadayev <dadayev95@gmail.com>
2022-09-20 09:05:24 +0200

    rating

---
+++ src/components/Feedback/Feedback.js
@@ -0,0 +1,18 @@
+import { Stack, Rating } from "@mui/material";
+import React, { useState} from "react";
+
+
+function Feedback () {
+
+
+    return (
+          <Stack spacing = {2}>
+              <Rating />
+      
+          </Stack>
+        
+     )
+}
+
+
+export default Feedback;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Feedback  from "../Feedback/Feedback.js";
 import Slider from "../Slider/slider";
+
+
 
 function Homepage (){
 return(
     <div>
     <Slider/>
+    <Feedback/>
     </div>
 )
 }
