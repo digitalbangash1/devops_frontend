d671a9b3bacc5fd51dbb3e3d244e5b65e5ae191a
ali <s183033@student.dtu.dk>
2022-09-21 13:19:55 +0200

    t

--- src/App.js
+++ src/App.js
@@ -5,12 +5,9 @@ import Home from './components/Home/Home'
 import Navbar from "./components/shared_view/Navbar";
 import Login from './components/login/Login';
 import Shoes from './components/products/shoes/Shoes'
-
 import Admin from './Pages/Admin/Admin';
 import Footer from "./components/shared_view/Footer";
-
 import Jeans from "./components/products/jeans/jeans";
-
 import Feedback from "./components/Feedback/Feedback";
 
 function App() {
@@ -18,15 +15,12 @@ function App() {
     <div className="App">
     <Navbar/>
      <Routes>
-       <Route path="/" element={<Home/>}/>
-       <Route path="/login" element={<Login/>}/>
+         <Route path="/" element={<Home/>}/>
+         <Route path="/login" element={<Login/>}/>
          <Route path="/shoes" element={<Shoes/>}/>
          <Route path="/jeans" element={<Jeans/>}/>
-
-
-          <Route path="/admin" element={<Admin/>}/>
-
-       <Route path="/Feedback" element={<Feedback/>}/>
+         <Route path="/admin" element={<Admin/>}/>
+         <Route path="/Feedback" element={<Feedback/>}/>
      </Routes>
      <Footer/>
     </div>
