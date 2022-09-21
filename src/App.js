506395f19414b8ee7ba7deeb53d2a82bde8409b6
ali <s183033@student.dtu.dk>
2022-09-21 13:18:42 +0200

    t

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Shoes from './components/products/shoes/Shoes'
 
 import Admin from './Pages/Admin/Admin';
 import Footer from "./components/shared_view/Footer";
-import Shoes from './components/products/shoes/Shoes';
+
 import Jeans from "./components/products/jeans/jeans";
 
 import Feedback from "./components/Feedback/Feedback";
