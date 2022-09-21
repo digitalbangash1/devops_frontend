9f5cdfd648fe362aced6b233b91f304b62cf818e
ali <s183033@student.dtu.dk>
2022-09-21 13:09:58 +0200

    t

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import './App.css';
 import {Route, Routes} from 'react-router-dom';
 import Home from './components/Home/Home'
 import Navbar from "./components/shared_view/Navbar";
-<<<<<<< HEAD
 import Login from './components/login/Login';
-=======
 import Shoes from './components/products/shoes/Shoes'
 import Admin from './Pages/Admin/Admin';
 
->>>>>>> devlopment
 
 function App() {
   return (
@@ -17,12 +14,9 @@ function App() {
     <Navbar/>
      <Routes>
        <Route path="/" element={<Home/>}/>
-<<<<<<< HEAD
        <Route path="/login" element={<Login/>}/>
-=======
          <Route path="/shoes" element={<Shoes/>}/>
           <Route path="/admin" element={<Admin/>}/>
->>>>>>> devlopment
      </Routes>
     </div>
   );
