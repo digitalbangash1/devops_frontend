8a06b6690763cf6df916e2ed9efccd9806d288f1
Strongside-87 <thomas.arildtoft@icloud.com>
2022-09-24 01:47:24 +0200

    Fixed footer bug

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Footer from "../shared_view/Footer";
+import Navbar from "../shared_view/Navbar";
 import Slider from "../Slider/slider";
 import Videoo from "./video";
 
@@ -11,8 +13,10 @@ function Homepage (){
 
 return(
     <div>
+            <Navbar />
             <Videoo/>
             <Slider/>
+            <Footer />
     </div>
 )
 }
