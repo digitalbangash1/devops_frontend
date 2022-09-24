8f64ece532cc46cd2cc660e76fbe6826cc5acf15
salim963 <salimdk97@gmail.com>
2022-09-24 19:08:11 +0200

    .

--- src/components/shared_view/Navbar.js
+++ src/components/shared_view/Navbar.js
@@ -16,7 +16,7 @@ function Navbar(){
 
             <div className="collapse navbar-collapse justify-content-end"  id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto px-2">
-                    <li className="nav-item active">
+                    <li className="nav-item">
                         
                         <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
                     </li>
