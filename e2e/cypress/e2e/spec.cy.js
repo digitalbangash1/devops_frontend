60310c13f0a53da38e2018a6112168d4696970b6
salim963 <salimdk97@gmail.com>
2022-11-02 23:05:59 +0100

    cy

--- e2e/cypress/e2e/spec.cy.js
+++ e2e/cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe('My First Test', () => {
   it('clicks the link "Feedback"', () => {
-    cy.visit('localhost:3000')
+    cy.visit('https://samat.admin.samat.diplomportal.dk/')
 
     cy.contains('Feedback').click()
   })
