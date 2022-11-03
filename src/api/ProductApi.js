8d026901d747f6f17a4d510bbbf6b6f8dfc76d46
Strongside-87 <thomas.arildtoft@icloud.com>
2022-11-03 14:43:25 +0100

    ProductAPI Merge

--- src/api/ProductApi.js
+++ src/api/ProductApi.js
@@ -1,21 +1,21 @@
 import axiosInstance from './axiosInstance';
 
 export function getProducts(){
-    const response = axiosInstance.get(`/product`);
+    const response = axiosInstance.get(`/products`);
     return response;
 }
 
 export function createProduct(product){
-    const response = axiosInstance.post(`/product`, product);
+    const response = axiosInstance.post(`/products`, product);
     return response;
 }
 
 export function updateProduct(id, product){
-    const response = axiosInstance.put(`/product/${id}`, product);
+    const response = axiosInstance.put(`/products/${id}`, product);
     return response;
 }
 
 export function deleteProduct(id){
-    const response = axiosInstance.delete(`/product/${id}`);
+    const response = axiosInstance.delete(`/products/${id}`);
     return response;
 }
