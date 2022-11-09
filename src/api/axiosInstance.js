f8211b6184614592d7cdd21f4640de389513307c
digitalbangash1 <s092512@student.dtu.dk>
2022-11-09 22:42:05 +0100

    product dynamic url implemented

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    //baseURL: 'https://backend-webshop.admin.samat.diplomportal.dk/' ,
-    baseURL: 'https://localhost:7181',
+    baseURL: 'https://backend-webshop.admin.samat.diplomportal.dk/' ,
+   // baseURL: 'https://localhost:7181',
     timeout: 10000
 });
