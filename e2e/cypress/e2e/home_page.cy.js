b7f89ef5a4cbb9d4c917d68f026c139db28d48e0
Muhammad Bangash <s092512@student.dtu.dk>
2022-11-22 15:55:22 +0100

    exception added to cypress

--- e2e/cypress/e2e/home_page.cy.js
+++ e2e/cypress/e2e/home_page.cy.js
@@ -1,6 +1,9 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false;
+});
 describe('The Home Page', () => {
     it('successfully loads', () => {
         cy.visit('/') // change URL to match your dev URL
-        Cypress.on('uncaught:exception', () => false)
+
     })
 })
