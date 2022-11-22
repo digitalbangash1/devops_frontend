8d4c485a8fced663976b322f161b5e4be204c99b
Muhammad Bangash <s092512@student.dtu.dk>
2022-11-22 16:46:29 +0100

    cypress stripe_betaling.cy.js

--- e2e/cypress/e2e/home_page.cy.js
+++ e2e/cypress/e2e/home_page.cy.js
@@ -1,6 +1,3 @@
-Cypress.on('uncaught:exception', (err, runnable) => {
-    return false;
-});
 describe('The Home Page', () => {
     it('successfully loads', () => {
         cy.visit('/') // change URL to match your dev URL
--- e2e/cypress/e2e/navbar_categories.cy.js
+++ e2e/cypress/e2e/navbar_categories.cy.js
@@ -1,4 +1,3 @@
-
 describe("Routes", () => {
     before(() => {
         cy.visit("/");
