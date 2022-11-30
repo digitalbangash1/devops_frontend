a65d41e4a4677cae064aad4d3104e8f7eaeff2e2
alidadayev <s172858@dtu.dk>
2022-11-30 13:24:12 +0100

    Rettelse

--- src/components/products/ProductDetails.js
+++ src/components/products/ProductDetails.js
@@ -39,29 +39,28 @@ export default function ProductDetails() {
 
     const navigate = useNavigate();
     const postData = () => {
-        axios.post(`https://backend-webshop.admin.samat.diplomportal.dk/feedback`, {
+        axios.post(`https://localhost:7181/feedback`, {
             review,
             rating,
             product_id,
-        }).then(() => {
-          window.location.reload(false);
+        }).then(() =>{
+            navigate('/feedback');
         });
     }
 
 
     const [APIData, setAPIData] = useState([]);
     useEffect(() => {
-        axios.get(`https://backend-webshop.admin.samat.diplomportal.dk/feedback`)
+        axios.get(`https://localhost:7181/feedback`)
             .then((response) => {
                 console.log(response.data)
                 setAPIData(response.data);
-
                 console.log("Our rating", setRating);
             })
     }, []);
 
     const setData = (data) => {
-        let {id, review, rating, product_id} = data;
+        let { id, review, rating, product_id} = data;
         localStorage.setItem('ID', id);
         localStorage.setItem('Review', review);
         localStorage.setItem('Rating', rating);
@@ -69,11 +68,11 @@ export default function ProductDetails() {
     }
 
     const getData = () => {
-        axios.get(`https://backend-webshop.admin.samat.diplomportal.dk/feedback`)
+        axios.get(`https://localhost:7181/feedback`)
             .then((getData) => {
                 setAPIData(getData.data);
             })
-    }
+        }
 
     return (
         <div>
@@ -86,49 +85,39 @@ export default function ProductDetails() {
                 <img src={product.imagelink} style={{width: '400px', borderRadius: '10px'}} alt=""/>
             </div>
 
-
-            <section className="vh-100" style={{backgroundColor: "#FFFFF"}}>
-
-
-                <MDBContainer className="py-5" style={{maxWidth: "1000px"}}>
+            <section className="vh-100" style={{ backgroundColor: "#FFFFF" }}>
+                <MDBContainer className="py-5" style={{ maxWidth: "1000px" }}>
                     <MDBRow className="justify-content-center">
-                        <MDBCol md="10" lg="8" xl="6">
-                            <MDBCard>
-                                <MDBCardBody className="p-4">
-                                    <div className="d-flex flex-start w-100">
-                                        <MDBCardImage
-                                            className="rounded-circle shadow-1-strong me-3"
-                                            src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(21).webp"
-                                            alt="avatar"
-                                            width="65"
-                                            height="65"
-                                        />
-                                        <div className="w-100">
-                                            <MDBTypography tag="h5">Add a comment</MDBTypography>
-                                            <div>
-                                                <Box sx={{'& > legend': {mt: 2},}}>
-                                                    <Typography component="legend">Rate this product</Typography>
-                                                    <Rating name="simple-controlled" value={value}
-                                                            onChange={(_event, newValue) => {
-                                                                setValue(newValue);
-                                                                setRating(newValue);
-                                                            }}/>
-                                                </Box>
-                                                <input placeholder='product_id'
-                                                       onChange={(e) => setProduct_id(e.target.value)}/>
-                                            </div>
-                                            <MDBTextArea label="What is your view?" rows={4}
-                                                         onChange={(e) => setReview(e.target.value)}/>
-                                            <div className="d-flex justify-content-between mt-3">
-                                                <MDBBtn color="danger" onClick={postData}> Send <MDBIcon fas
-                                                                                                          icon="long-arrow-alt-right ms-1"/>
-                                                </MDBBtn>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </MDBCardBody>
-                            </MDBCard>
-                        </MDBCol>
+                    <MDBCol md="10" lg="8" xl="6">
+                        <MDBCard>
+                        <MDBCardBody className="p-4">
+                            <div className="d-flex flex-start w-100">
+                            <MDBCardImage
+                                className="rounded-circle shadow-1-strong me-3"
+                                src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(21).webp"
+                                alt="avatar"
+                                width="65"
+                                height="65"
+                            />
+                            <div className="w-100">
+                                <MDBTypography tag="h5">Add a comment</MDBTypography>
+                                <div>
+                                <Box sx={{ '& > legend': { mt: 2 },}}>
+                                    <Typography component="legend">Rate this product</Typography>
+                                    <Rating name="simple-controlled" value={value} onChange={(_event, newValue) => {setValue(newValue); setRating(newValue);}}/>
+                                </Box>
+                                <input placeholder='product_id' onChange={(e) => setProduct_id(e.target.value)}/>
+                                </div>
+                                <MDBTextArea label="What is your view?" rows={4} onChange={(e) => setReview(e.target.value)}/>
+                                <div className="d-flex justify-content-between mt-3">
+                                <MDBBtn color="danger" onClick={postData} > Send <MDBIcon fas icon="long-arrow-alt-right ms-1" />
+                                </MDBBtn>
+                                </div>
+                            </div>
+                            </div>
+                        </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
                     </MDBRow>
                 </MDBContainer>
 
@@ -143,22 +132,20 @@ export default function ProductDetails() {
                         </Table.Header>
 
                         <Table.Body>
-                            {product.feedback?.map((feedback,index) => {
+                            {APIData.map((data) => {
                                 return (
 
-                                    <Table.Row key = {index}>
-                                        <Table.Cell>{feedback.review}</Table.Cell>
-                                        <Table.Cell>{feedback.rating}</Table.Cell>
-                                        <Table.Cell>{feedback.product_id}</Table.Cell>
+                                    <Table.Row>
+                                        <Table.Cell>{data.review}</Table.Cell>
+                                        <Table.Cell>{data.rating}</Table.Cell>
+                                        <Table.Cell>{data.product_id}</Table.Cell>
                                     </Table.Row>
                                 )
                             })}
                         </Table.Body>
                     </Table>
                 </div>
-            </section>
-
-
+                </section>
         </div>
     );
 }
